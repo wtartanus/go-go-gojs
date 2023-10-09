63235e0a77ee14a96338e01d387614fda4c311b8
Wojciech Tartanus <wojciech.tartanus@synergycodes.com>
2023-10-09 19:03:25 +0200

    basic-lesson-8-homework

--- src/gojs/utils/exportToPng.ts
+++ src/gojs/utils/exportToPng.ts
@@ -1,4 +1,5 @@
 import * as go from 'gojs';
+import mergeImages from 'merge-images';
 
 declare global {
     interface Navigator {
@@ -28,12 +29,35 @@ const callback = (blob: Blob) => {
     });
 };
 
-export const exportToPng = (diagram: go.Diagram) => {
-    diagram.makeImageData({
-        background: 'white',
-        returnType: 'blob',
-        scale: 1,
-        padding: 10,
-        callback
-    })
+export const exportToPng = async (diagram: go.Diagram) => {
+    const CONTENT_MAX_SIZE = 2000;
+    const {
+        width: diagramWidth,
+        height: diagramHeight 
+    } = diagram.documentBounds;
+    const imagesCount = Math.ceil(diagramWidth / CONTENT_MAX_SIZE);
+    
+    const images = Array.from({ length: imagesCount }, (_, i) => {
+        const x = i * CONTENT_MAX_SIZE;
+        const { src } = diagram.makeImage({
+            position: new go.Point(x, 0),
+            size: new go.Size(CONTENT_MAX_SIZE, diagramHeight),
+            scale: 1,
+            background: 'white',
+        });
+
+        return { src, x, y: 0}
+    });
+  
+    const mergedUrl = await mergeImages(images, { width: diagramWidth, height: diagramHeight });
+  
+    const binaryData = atob(mergedUrl.split(',')[1]);
+    const uint8Array = new Uint8Array(binaryData.length);
+    
+    for (let i = 0; i < binaryData.length; i++) {
+      uint8Array[i] = binaryData.charCodeAt(i);
+    }
+
+    const blob = new Blob([uint8Array], { type: 'image/png' });
+    callback(blob);
 }
